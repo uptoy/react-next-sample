3bc79c9e6c591739ba9f23f9d6e4cd044bd48cc3
uptoy <u24to45y@gmail.com>
2022-07-14 00:34:52 +0900

    [add] fetch pokemon api

---
+++ 07.hooks/02.sample-3-reactive-pokemon/reactive-pokemon/src/App.tsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState, useMemo } from 'react'
+import { Pokemon, getAll, getByName } from './API'
+
+import './styles.css'
+//https://pokeapi.co/api/v2/pokemon/1
+
+const calculatePower = (pokemon: Pokemon) =>
+  pokemon.hp +
+  pokemon.attack +
+  pokemon.defense +
+  pokemon.special_attack +
+  pokemon.special_defense +
+  pokemon.speed
+
+const PokemonTable: React.FunctionComponent<{
+  pokemon: Pokemon[]
+}> = ({ pokemon }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <td>ID</td>
+          <td>Name</td>
+          <td>Type</td>
+          <td colSpan={6}>Stats</td>
+        </tr>
+      </thead>
+      <tbody>
+        {pokemon.map((p) => (
+          <tr key={p.id}>
+            <td>{p.id}</td>
+            <td>{p.name}</td>
+            <td>{p.type.join(',')}</td>
+            <td>{p.hp}</td>
+            <td>{p.attack}</td>
+            <td>{p.defense}</td>
+            <td>{p.special_attack}</td>
+            <td>{p.special_defense}</td>
+            <td>{p.speed}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
+
+export default function App() {
+  const [pokemon, setPokemon] = useState<Pokemon[]>([])
+  useEffect(() => {
+    getAll().then(setPokemon)
+  }, [])
+  return (
+    <div>
+      <div className="top-bar">
+        <div>Search</div>
+        <input type="text"></input>
+        <div>Power threshold</div>
+        <input type="text"></input>
+        <div>Count over threshold: </div>
+      </div>
+      <div className="two-column">
+        <PokemonTable pokemon={pokemon} />
+        <div>
+          <div>Min: </div>
+          <div>Max: </div>
+        </div>
+      </div>
+    </div>
+  )
+}
